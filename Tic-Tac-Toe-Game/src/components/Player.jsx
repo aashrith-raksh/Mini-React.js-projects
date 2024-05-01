9685c72704ba7f7f6f8cf4bb80a2053f853e3a5d
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-05-01 18:52:13 +0530

    added editable player names section

---
+++ Tic-Tac-Toe-Game/src/components/Player.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentPlayerName, setcCurrentPlayerName] = useState(initialName);
+  let editablePlayerName = (
+    <span className="player-name">{currentPlayerName}</span>
+  );
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        required
+        value={currentPlayerName}
+        onChange={handleInputChage}
+      ></input>
+    );
+  }
+
+  function handleInputChage(e) {
+    let userInput = e.target.value;
+    setcCurrentPlayerName(userInput);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-logo">{symbol}</span>
+      </span>
+      <button onClick={() => setIsEditing((prev) => !prev)}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
+    </li>
+  );
+}
