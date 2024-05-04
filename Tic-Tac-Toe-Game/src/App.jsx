13083e999ddf6564fee6ef37145aa23b421bfc65
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-05-04 17:06:16 +0530

    added log component optimized game-board state management

--- Tic-Tac-Toe-Game/src/App.jsx
+++ Tic-Tac-Toe-Game/src/App.jsx
@@ -1,18 +1,56 @@
+import { useCallback, useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+import Log from "./components/Log";
 
 function App() {
-  
+  const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const handleSelectSquare = useCallback((rowIndex, colIndex) => {
+    setActivePlayer(activePlayer == "X" ? "O" : "X");
+    setGameTurns((prevGameTurns) => {
+      let currentPlayer = "X";
+
+      if (prevGameTurns.length > 0 && prevGameTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      const updatedGameTurns = [
+        {
+          square: {
+            row: rowIndex,
+            col: colIndex,
+          },
+          player: currentPlayer,
+        },
+        ...prevGameTurns,
+      ];
+
+      return updatedGameTurns;
+    });
+  });
 
   return (
-    <div id="game-container">
-      <ol id="players">
-        <Player initialName={"Player 1"} symbol={"X"} />
-        <Player initialName={"Player 2"} symbol={"O"} />
-      </ol>
-      <GameBoard/>
-    </div>
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName={"Player 1"}
+            symbol={"X"}
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName={"Player 2"}
+            symbol={"O"}
+            isActive={activePlayer === "O"}
+          />
+        </ol>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+      </div>
+      <Log turns={gameTurns} />
+    </main>
   );
 }
 
-export default App
+export default App;
--- Tic-Tac-Toe-Game/src/components/GameBoard.jsx
+++ Tic-Tac-Toe-Game/src/components/GameBoard.jsx
@@ -1,35 +1,47 @@
 import { useState } from "react";
 
-const BOARD = [
+const INITIAL_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard() {
-    const [initialGameBoard,setInitialGameBoard] = useState(BOARD);
+export default function GameBoard({onSelectSquare, turns}) {
+    // const [initialGameBoard,setInitialGameBoard] = useState(BOARD);
 
 
-    function handleClick(rowIndex, colIndex){
-        console.log("rowIndex: " + rowIndex)
-        console.log("colIndex: " + colIndex)
-        setInitialGameBoard((prevBoard) => {
-            const colCopy = prevBoard.map(row => [...row]);
-            const updatedBoard = [...colCopy];
+    // function handleClick(rowIndex, colIndex){
+    //     console.log("rowIndex: " + rowIndex)
+    //     console.log("colIndex: " + colIndex)
+    //     setInitialGameBoard((prevBoard) => {
+    //         const colCopy = prevBoard.map(row => [...row]);
+    //         const updatedBoard = [...colCopy];
 
-            updatedBoard[rowIndex][colIndex] = 'X';
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
 
-            return updatedBoard;
-        })
+    //         return updatedBoard;
+    //     })
+
+    //     onSelectSquare(activePlayerSymbol)
+    // }
+
+    let gameBoard = INITIAL_BOARD;
+    
+    for(const turn of turns){
+      console.log('turn:', turn)
+      const {square, player} = turn;
+      const {row, col}= square;
+
+      gameBoard[row][col] = player;
     }
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => {
+      {gameBoard.map((row, rowIndex) => {
          return <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => {
                return <li key={colIndex}>
-                <button onClick={() => handleClick(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
               </li>;
             })}
           </ol>
