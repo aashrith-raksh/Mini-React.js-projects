890f3f76c4d317e8af94664282957896d461b126
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-06-06 17:19:06 +0530

    added functionality for highlighting correct and wrong answers

--- Quiz-App/src/components/Quiz.jsx
+++ Quiz-App/src/components/Quiz.jsx
@@ -1,11 +1,14 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import QUESTIONS from "../../questions";
 import quizCompleteImg from "../assets/quiz-complete.png";
 import Timer from "./TImer";
 
 export default function Quiz() {
+  let shuffledAnswers = useRef();
+  const [answerState, setAnswerState] = useState('');
+  if(answerState == '') shuffledAnswers.current = undefined
   const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuestionIndex = userAnswers.length;
+  const activeQuestionIndex = answerState === '' ? userAnswers.length: userAnswers.length -1;
   /*
         if the user has already answered 5 questoins
         userAnswers contains 5 answers(0 --> 4)
@@ -19,9 +22,40 @@ export default function Quiz() {
   const handleSelectAnswer = useCallback(function handleSelectAnswer(
     selectedAnswer
   ) {
+    if(selectedAnswer === null){
+      setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
+      setAnswerState('');
+      return;
+    }
+
+    setAnswerState('answered');
     setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
+
+    
+
+    // 1 sec timer after which answer will be set to correct or wrong
+    setTimeout(() => {
+      if(selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]){
+        setAnswerState('correct');
+        // answerState = 'correct'
+        }else{
+        setAnswerState('wrong');
+        // answerState = 'wrong'
+      }
+
+      /*
+        after 1 sec collapses, 
+        answerState will be set to either 'correct' OR 'worng'
+        
+        Then the below 2 sec timer will be set.
+        This will reset the answerState to '' (EMPTY STRING) 
+      */
+      setTimeout(() => {
+        setAnswerState('');
+      },1000)
+    }, 1000);
   },
-  []);
+  [activeQuestionIndex]);
 
   const handleSkipAnswer = useCallback(() => {
     handleSelectAnswer(null);
@@ -37,23 +71,37 @@ export default function Quiz() {
   }
 
   const activeQuestion = QUESTIONS[activeQuestionIndex];
-  const shuffledAnswers = [...activeQuestion.answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5);
+  if(!shuffledAnswers.current){
+    shuffledAnswers.current = [...activeQuestion.answers];
+    shuffledAnswers.current.sort(() => Math.random() - 0.5);
+  }
 
   return (
     <div id="quiz">
       <div id="question">
         <Timer
-          timeOut={5000}
+          timeOut={10000}
           onTimeOut={handleSkipAnswer}
           key={activeQuestionIndex}
         />
         <h2>{activeQuestion.text}</h2>
         <ul id="answers">
-          {shuffledAnswers.map((answer) => {
+          {shuffledAnswers.current.map((answer) => {
+            let cssClass = '';
+
+            const isSelected = answer === userAnswers[userAnswers.length-1];
+
+            if(answerState === 'answered' && isSelected){
+              cssClass = 'selected'
+            }
+
+            if((answerState === 'correct' || answerState === 'wrong') && isSelected){
+              cssClass = answerState;
+            }
+
             return (
               <li key={answer} className="answer">
-                <button onClick={() => handleSelectAnswer(answer)}>
+                <button onClick={() => handleSelectAnswer(answer)} className={cssClass}>
                   {answer}
                 </button>
               </li>
--- Quiz-App/src/components/Timer.jsx
+++ Quiz-App/src/components/Timer.jsx
@@ -1,30 +1,31 @@
 import { useEffect, useState } from "react";
-console.log('\n\ntimer.jsx render\n--------------------\n')
+console.log("\n\ntimer.jsx render\n--------------------\n");
 
 export default function Timer({ onTimeOut, timeOut }) {
-    console.log('\n\n------ Timer Component Render ----------')
+  console.log("\n\n------ Timer Component Render ----------");
 
-  const [timeRemaining, setTimeRemaining] = useState(timeOut)
-
-  useEffect(() =>{
-    console.log('timeout set')
-      const questionTimer = setTimeout(onTimeOut, timeOut);
-      return () => {
-        console.log('timer cleared')
-        clearTimeout(questionTimer)}
-  }, [onTimeOut, timeOut])
+  const [timeRemaining, setTimeRemaining] = useState(timeOut);
 
   useEffect(() => {
-    console.log('interval set')
+    console.log("\nSETTING TIMEOUT");
+    const questionTimer = setTimeout(onTimeOut, timeOut);
+    return () => {
+      console.log("CLEARING TIMOUT");
+      clearTimeout(questionTimer);
+    };
+  }, [onTimeOut, timeOut]);
+
+  useEffect(() => {
+    console.log("SETTING INTERVAL");
     const interval = setInterval(() => {
       setTimeRemaining((prevTime) => prevTime - 1000);
     }, 1000);
 
     return () => {
-        console.log('interval cleared')
-        clearInterval(interval)
-    }
+      console.log("CLEARING INTERVAL");
+      clearInterval(interval);
+    };
   }, []);
 
-  return <progress id="question-time" value={timeRemaining} max={timeOut}/>;
+  return <progress id="question-time" value={timeRemaining} max={timeOut} />;
 }
