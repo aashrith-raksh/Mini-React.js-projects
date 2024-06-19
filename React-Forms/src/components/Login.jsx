e1406fec4c051f2a063a006edd01c934aaaab1cc
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-06-19 19:32:40 +0530

    input validation for email and password

--- React-Forms/src/components/Login.jsx
+++ React-Forms/src/components/Login.jsx
@@ -1,4 +1,33 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [inputState, setInputState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [emailOutOfFocus, setEmailOutOfFocus] = useState(false);
+
+  const emailIsInvalid =
+    emailOutOfFocus &&
+    inputState.email.length > 0 &&
+    !inputState.email.includes("@");
+
+  function handleInputChange(event) {
+    const identifier = event.target.id;
+    setInputState((prevInput) => {
+      return {
+        ...prevInput,
+        [identifier]: event.target.value,
+      };
+    });
+
+    setEmailOutOfFocus(false);
+  }
+
+  function handleOutOfFocus() {
+    setEmailOutOfFocus(true);
+  }
   return (
     <form>
       <h2>Login</h2>
@@ -6,12 +35,26 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleInputChange}
+            onBlur={handleOutOfFocus}
+            value={inputState.email}
+          />
+          <div>{emailIsInvalid && <p>Please enter a valid email</p>}</div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={handleInputChange}
+            value={inputState.password}
+          />
         </div>
       </div>
 
