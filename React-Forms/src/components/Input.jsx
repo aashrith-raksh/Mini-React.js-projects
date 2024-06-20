44acd7f6e6fed5979e8f2690a79bcc451a067945
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-06-20 19:59:30 +0530

    styled input error messages

--- React-Forms/src/components/Input.jsx
+++ React-Forms/src/components/Input.jsx
@@ -4,7 +4,7 @@ export default function Input({ label, id, error, ...props }) {
     <div className="control no-margin">
       <label htmlFor={id}>{label}</label>
       <input id={id} {...props} />
-      <div>{error && <p>{error}</p>}</div>
+      <div className="control-error">{error && <p>{error}</p>}</div>
     </div>
   );
 }
